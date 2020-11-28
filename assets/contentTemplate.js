4098e9de8b314a752f12afc060a7088b0fbad58d
tt4623 <sltebr33zy@gmail.com>
2020-11-27 22:53:25 -0600

    fixed template for markdown format

--- assets/contentTemplate.js
+++ assets/contentTemplate.js
@@ -1,30 +1,29 @@
 // readme layout
-const content =
-  `
-  # ${data.title}
-  ***
-  ## Description
-  ***
-  ${data.desc}
-  ## Table of Contents
-  1. [Installation] (#//headline of section)
-  2. [Usage] ()
-  3. [Credits] ()
-  4. [License] ()
-  ***
-  ## Installation
-  ***
-  ${data.installationInstructions}
-  ## Usage
-  ***
-  ${data.useInfo}
-  ## Credits
-  ***
-  ${data.credits}
-  ## License
-  ***
-  ${data.license}
-  `
-  // ... more
+const createTemplate = data => {
+  return (
+`## ${data.title}
+***
 
-module.exports = content;
+### Description
+***
+${data.desc}
+
+### Installation
+***
+${data.installationInstructions}
+
+### Usage
+***
+${data.useInfo}
+
+### Credits
+***
+${data.credits}
+
+### License
+***
+${data.license}`
+// ... more
+  )
+}
+module.exports = createTemplate;
